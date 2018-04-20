48909ff34d8197c17176735feeab473209119a50
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2018-04-20 18:30:20 +0200

    Added comments to code, make structure clearer

--- compose/kafka_stack/src/dc-service.py
+++ compose/kafka_stack/src/dc-service.py
@@ -11,28 +11,13 @@ from flask import Flask, jsonify, request
 from redis import Redis
 from logstash import TCPLogstashHandler
 
-# confluent_kafka is based on librdkafka, details in requirements.txt
-# from confluent_kafka import Consumer, KafkaError
-
-# Why using a kafka to logstash adapter, while there is a plugin?
-# Because there are measurements, as well as observations valid as SensorThings result.
-# Kafka Adapters seems to use only one topic
-# ID mapping is pretty much straightforward with a python script
-
-
 __date__ = "20 April 2018"
 __version__ = "1.2"
 __email__ = "christoph.schranz@salzburgresearch.at"
 __status__ = "Development"
-__desc__ = """This program manages kafka topics on the broker."""
+__desc__ = """The datachannel services manages kafka topics on the broker, clones the datachannel contract onto the provided 
+functionality of SensorThings and provides API."""
 
-#
-# # kafka parameters
-# # topics and servers should be of the form: "topic1,topic2,..."
-# KAFKA_TOPICS = "SensorData"  # TODO can be set as env, Also works for the logstash pipeline index filter
-# BOOTSTRAP_SERVERS_default = 'il061,il062,il063'
-#
-# # "iot86" for local testing. In case of any data losses, temporarily use another group-id until all data is load.
 # KAFKA_GROUP_ID = "iot86"  # use il060 if used in docker swarm
 # # If executed locally with python, the KAFKA_GROUP_ID won't be changed
 # KAFKA_GROUP_ID = os.getenv('KAFKA_GROUP_ID', KAFKA_GROUP_ID)  # overwrite iot86 by envfile ID=il060
@@ -41,24 +26,24 @@ __desc__ = """This program manages kafka topics on the broker."""
 # # logstash parameters
 # HOST_default = KAFKA_GROUP_ID  # 'il060'   # use the local endpoint: equals hostname
 # PORT_default = 5000
+
+# Kafka parameters
 STATUS_FILE = "status.log"
 ZOOKEEPER_HOST = "localhost:2181"
 NUMBER_OF_REPLICAS = 1
 NUMBER_OF_PARTITIONS = 1
-
 RETENTION_TIME = 6  # in months
-
-
-logger = logging.getLogger('kafka_manager.logging')
-logger.setLevel(os.getenv('LOG_LEVEL', logging.INFO))
-console_logger = logging.StreamHandler(stream=sys.stdout)
-logger.addHandler(console_logger)
-logger.info("Started data-channel service")
 
 # Sensorthings parameters
 # ST_SERVER = "http://localhost:8084/v1.0/"  # reachable only from host outside docker
 ST_SERVER = "http://gost:8080/v1.0/"  # GOST server is reachable within kafka stack with that
 # REFRESH_MAPPING_EVERY = 5 * 60  # in seconds
+
+logger = logging.getLogger('kafka_manager.logging')
+logger.setLevel(os.getenv('LOG_LEVEL', logging.INFO))
+console_logger = logging.StreamHandler(stream=sys.stdout)
+logger.addHandler(console_logger)
+logger.info("Started data-channel service with hosts: {}, {}".format(ZOOKEEPER_HOST, ST_SERVER))
 
 # webservice setup
 app = Flask(__name__)
@@ -72,12 +57,12 @@ logger.info("Added flask API on port {}.".format(6379))
 def print_adapter_status():
     """
     This function is called by a sebserver request and prints the current meta information.
-    :return:
+    :return: status
     """
     try:
         with open(STATUS_FILE) as f:
             adapter_status = json.loads(f.read())
-    except:
+    except FileNotFoundError:
         adapter_status = {"application": "kafka-manager",
                           "status": "running",
                           "topics": list_topics()}
@@ -85,56 +70,22 @@ def print_adapter_status():
 
 
 def list_topics():
+    """
+    This function returns all kafka topics.
+    :return: a list of registered topics
+    """
     pipe = os.popen("kafka-topics --list --zookeeper {}".format(ZOOKEEPER_HOST))
     topics = pipe.read().split("\n")
     return topics
 
 
-# respond to invalid request
-@app.route('/create')
-@app.route('/create/<company>')
-@app.route('/create/<company>/<machine>')
-def create_invalid():
-    response = "creation of sensor must be of the form: '/create/<company>/<machine>/<sensor>'"
-    return jsonify(response)
-
-
-# http://0.0.0.0:3033/create_sensor
-# header: application/json
-@app.route('/create_sensor', methods=['GET', 'POST'])
-def create_topic_api():
-    data = json.loads(request.data)
-    company = data["sensor"].get("company", "")
-    system = data["sensor"].get("system", "")
-    sensor = data["sensor"].get("sensor", "")
-    result = data["sensor"].get("result", "")
-    topic_name = ".".join([str(i) for i in ["eu", company, system, sensor]])[:-1]
-    logger.info("Added sensor with topic {}, result is of type {}.".format(topic_name, type(result)))
-
-    cmd = """kafka-topics --create --zookeeper {zoo} --topic {tpc}
- --replication-factor {rep} --partitions 1 --config cleanup.policy=compact --config flush.ms=60000
- --config retention.ms={ret}
-""".format(zoo=ZOOKEEPER_HOST, tpc=topic_name, rep=NUMBER_OF_REPLICAS,
-           ret=RETENTION_TIME*31*24*3600*1000).replace("\n", "")
-    pipe = os.popen(cmd)
-    response = pipe.read()
-    if "\nCreated" in response:
-        response = "Created topic" + response.split("\nCreated topic")[-1].\
-            replace("\n", "").replace('"', "")  # [:-1]
-    elif "Error" in response and "already exists." in response:
-        # Topic already exists
-        logger.warning("Couldn't create sensor with topic {}, topic already exists.".format(topic_name))
-        return jsonify({"Error, sensor already exists": str(response)}), 403
-    else:
-        logger.warning("Couldn't create sensor with topic {}".format(topic_name))
-        return jsonify({"Couldn't create sensor with topic": str(response)}), 404
-    logger.info("Added sensor with topic {}, result is of type {}.".format(topic_name, type(result)))
-    logger.info(response)
-    return jsonify(data)
-
-
 def reassemble_kafka_from_st():
-    time.sleep(20)  # wait for kafka and GOST server
+    """
+    When the broker is started, this function is called to restore the kafka topics from the stored
+    SensorThings contracts.
+    :return:
+    """
+    time.sleep(15)  # wait for kafka and GOST server
     print("Trying to restore kafka topics from SensorThings")
 
     thing_id = 0
@@ -146,38 +97,37 @@ def reassemble_kafka_from_st():
             .format(id=thing_id))
         if response.status_code not in [200]:
             break
-        logger.info(response)
-        thing = yaml.safe_load(response.text)
 
-        channelID = thing.get("@iot.id")
-        companyID = thing.get("properties").get("owner")
+        thing = yaml.safe_load(response.text)  # load safely because unicode may cause errors
+        channel_id = thing.get("@iot.id")
+        company_id = thing.get("properties").get("owner")
 
-        topic_name = "eu.channelID_{chID}.companyID_{compID}".format(chID=channelID, compID=companyID)
+        topic_name = "eu.channelID_{chID}.companyID_{compID}".format(chID=channel_id, compID=company_id)
         topic_name = re.sub("[^a-zA-Z.0-9_-]+", "", topic_name.replace(" ", "-"))
-        logger.info(topic_name)
 
         status = create_topic(topic_name)
         stati.append(status)
-        logger.info("dc-service successfully restored kafka topic {} from Sensorthings".format(topic_name))
 
-    tracebacks = [status for status in stati if "Successfully created topic" != status]
-    if tracebacks is not []:
-        logger.warning("dc-service encountered errors while restoring topics: {}".format(tracebacks))
+    logger.info("dc-service successfully restored all kafka topics from Sensorthings")
 
 
 def get_topic_name(payload):
-    # is of the form eu.owner.thingname, only alphanumerics and .-_ are allowed
-    # topic_name = "eu."+str(payload["properties"]["owner"])+"."+str(payload["name"])
-
-    # is of the form eu.ChannelID_<channelID>.CompanyID_<companyID>
-    channelID = payload.get("@iot.id")
-    companyID = payload.get("properties").get("owner")
-    topic_name = "eu.ChannelID_{chID}.CompanyID_{compID}".format(chID=channelID, compID=companyID)
-    topic_name = re.sub("[^a-zA-Z.0-9_-]+", "", topic_name.replace(" ", "-"))
+    """
+    Given a payload, this function creates an unique name that is valid for creating a kafka topic.
+    :return: a kafka-topic name form eu.ChannelID_<channel_id>.CompanyID_<companyID>
+    """
+    channel_id = payload.get("@iot.id")
+    company_id = payload.get("properties").get("owner")
+    topic_name = "eu.channelID_{chID}.companyID_{compID}".format(chID=channel_id, compID=company_id)
+    topic_name = re.sub("[^a-zA-Z.0-9_-]+", "", topic_name.replace(" ", "-"))  # only alphanumerics and _.- are allowed
     return topic_name
 
 
 def create_topic(topic_name):
+    """
+    Given a kafka topic name, this function calls the internal cli to create a topic.
+    :return: status string. Success, topic already exists or an other error message
+    """
     cmd = """kafka-topics --create --zookeeper {zoo} --topic {tpc}
  --replication-factor {rep} --partitions {par} --config cleanup.policy=compact --config flush.ms=60000
  --config retention.ms={ret}
@@ -185,10 +135,10 @@ def create_topic(topic_name):
            ret=RETENTION_TIME*31*24*3600*1000).replace("\n", "")
     pipe = os.popen(cmd)
     response = pipe.read()
-    print(response)
+
     if "\nCreated" in response:
-        response = "Created topic" + response.split("\nCreated topic")[-1].\
-            replace("\n", "").replace('"', "")
+        # Filter line of form: Created topic "eu.channelID_1.companyID_ownername".
+        response = response.split("\n")[-2]
     elif "Error" in response and "already exists." in response:  # Topic already exists
         logger.warning("Couldn't create sensor with topic {}, topic already exists.".format(topic_name))
         return "Error, instance already exists"
@@ -204,23 +154,27 @@ def create_topic(topic_name):
 # payload is the contract where sensors are specified
 @app.route('/submit_contract', methods=['GET', 'POST'])
 def submit_contract():
-    logger.info("Received contract")
-    # payload = json.loads(request.data)
+    """
+    API for submitting a data-channel contract
+    :return: Payload of the SensorThing call, or traceback code.
+    """
+    logger.info("Received new contract")
 
     headers = {'content-type': 'application/json'}
     response = requests.request("POST", ST_SERVER + "Things", data=request.data, headers=headers)
-    payload = json.loads(response.text)
 
-    if response.status_code not in [200, 202]:
+    if response.status_code not in [200, 201, 202]:
         logger.warning("Posting contract to SensorThings failed with status {}".format(response.status_code))
         logger.warning("Couldn't create instance {}".format(response.text))
         return jsonify({"Couldn't create instance {}".format(response.text)}), 409
 
+    payload = yaml.safe_load(response.text)
     logger.info("Added topic with name {} to SensorThings".format(payload.get("name")))
 
     topic_name = get_topic_name(payload)
     status = create_topic(topic_name)
-    logger.info("Created kafka topic with name: {}, returned status {}".format(topic_name, status))
+    logger.info("Created kafka topic with name {tpc} on system {sys}, returned status: {stat}"
+                .format(tpc=topic_name, sys=payload.get("name"), stat=status))
 
     return jsonify(payload)
 
